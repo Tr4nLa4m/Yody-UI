138e61ab7ccb324f89d399332bb25d0476682170
tr4nlamVnist <lamtn.vnist@gmail.com>
2022-08-20 15:40:38 +0700

    updated UI

---
+++ Yody/yody-ui/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='upload'>
+        <div className='upload_content'>
+          <label for="input-img" className='preview'>
+            
+          </label>
+        </div>
+      </div>
+      <div className='results'></div>
+    </div>
+  );
+}
+
+export default App;
